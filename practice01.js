59ae8a1b5f13ff791e7f53b9f5f61cafffff0d8a
Maniket Kumar <sonuanand644@gmail.com>
2022-06-24 09:11:20 +0530

    updated

--- practice01.js
+++ practice01.js
@@ -41,3 +41,45 @@ function x(){
 }
 console.log(a);
 console.log(x());
+
+/*
+Execution context
+1. Variavle environemnt (Memory allocation)
+2. Thread of execution (code execution)
+
+or we can say JS has 2 phase
+1. Memory creation.
+2. code execution phase.
+*/
+
+console.log(sum3());
+console.log(sum4);
+console.log(sum5)
+function sum3()
+{
+    return 2+3;
+}
+var sum4 = function(){
+    return 2+3;
+}
+
+//Arrow function...
+var sum5 = () =>{
+    return 2+3;
+};
+
+/* Uses of let and const `let` is a signal that the variable may be reassigned
+`const` is a signal that the identifier won't be reassigned. */
+
+
+
+
+
+
+
+
+
+
+
+
+
