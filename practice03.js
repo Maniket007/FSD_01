5339b38904147f791252a14a76b4b9149310815c
Maniket Kumar <sonuanand644@gmail.com>
2022-06-25 19:10:45 +0530

    updated

---
+++ practice03.js
@@ -0,0 +1,16 @@
+//React disadvantages is... it is manage by client side rendering...
+//React is a component based design...
+//React setup is deployed to ---- code sandbox..
+//we have two types of rendering 
+//  1. client side rendering ...---> Browser will manage all of the UI part...data will manage to the server..
+//  2. server side rendering ...---> initial loading..means for user it is slow it is taking time to load the data..
+
+
+
+
+
+
+
+
+
+
