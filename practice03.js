f7a7ff14545c8af5506964bd47641018db0e3bc9
Maniket Kumar <sonuanand644@gmail.com>
2022-06-26 14:00:27 +0530

    updated

--- practice03.js
+++ practice03.js
@@ -10,6 +10,51 @@
 
 //Props----- Pass the data from parent component to child component....
 
+//Below code are the syntax for code in react (codesandbox...)
+
+
+//App.js
+import "./styles.css";
+import Welcome from "./Welcome.js";
+function App() {
+  return (
+    <div className = "app">
+      <h1 style = {{color:"red"}}>Heloo world</h1>
+      <Welcome name = "Manik" age="10"/>
+      <Welcome name = "Abhi" age="20"/>
+      <Welcome name = "Pattern" age="30"/>
+      <Welcome name = "Money" age="40"/>
+      <Welcome name = "Heist" age="50"/>
+      <Welcome name = "Campa" age="60"/>
+      
+    </div>
+  );
+}
+export default App;
+
+//Welcome.js
+function Welcome(props){
+    return <h2>Welcome to this session {props.name} and my age is {props.age}</h2>
+  }
+  export default Welcome;
+
+//Index.js
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App/>);
+
+//Style.css
+.app {
+    font-family: sans-serif;
+    text-align: center;
+  }
+  
 
 
 
